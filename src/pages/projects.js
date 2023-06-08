593696e3f68e4d276b86422c66c6bb03c3b9ce7a
Ryan Abeysinghe <ryanabey@terpmail.umd.edu>
2023-06-08 15:20:16 -0400

    adding project cards

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,9 +1,85 @@
-import React from 'react'
+import React from 'react';
+import Head from 'next/head';
+import Layout from '@/components/Layout';
+import TextAnimation from '@/components/TextAnimation'
+import Link from 'next/link';
+import { GithubIcon } from '@/components/SocialMediaIcons'
+import Image from 'next/image';
+import RandSConstructionsWebsiteProj from '../../public/images/projects/RandSConstructionsWebsiteProj.png';
+
+const FeaturedProject = ({ projectImg, projectType, projectTitle, projectDescription, gitHubURL, projectURL }) => {
+
+  return (
+    <article className='w-full flex items-center justify-between rounded-3xl border border-solid border-dark bg-light shadow-2xl p-12'>
+      <Link href={projectURL} target={"_blank"} className='w-1/2 cursor-pointer overflow-hidden rounded-lg'>
+        <Image src={projectImg} alt={projectTitle} className='w-full h-auto' />
+      </Link>
+
+      <div className='w-1/2 flex flex-col items-start justify-between pl-6'>
+        <span className='text-primary font-medium text-xl'>{projectType}</span>
+        <Link href={projectURL} target={"_blank"} className='hover:underline underline-offset-2'>
+          <h2 className='mt-2 w-full text-left text-3xl font-bold'>{projectTitle}</h2>
+        </Link>
+        <p className='my-2 font-medium text-dark'>{projectDescription}</p>
+
+        <div className='mt-2 flex items-center'>
+          <Link href={projectURL} target={"_blank"} 
+          className='rounded-lg p-2 px-6 bg-dark text-light text-lg font-semibold hover:bg-light hover:text-dark border-2 border-solid border-transparent hover:border-dark duration-700'>Visit Project</Link>
+          <Link href={gitHubURL} target={"_blank"} className='w-10 ml-4'> <GithubIcon /> </Link>
+        </div>
+
+      </div>
+
+    </article>
+  );
+};
 
 const projects = () => {
   return (
-    <div>projects</div>
-  )
-}
+    <>
+      <Head>
+        <title>Projects | Ryan Abeysinghe</title>
+        <meta name='description' content='About Ryan Abeysinghe' />
+      </Head>
+      <main className='w-full mb-16 flex flex-col items-center justify-center'>
+        <Layout className='pt-16'>
+          <TextAnimation text="Inspire Change Through Design!" className='!text-7xl mb-16' />
 
-export default projects
+          <div className='grid grid-cols-12 gap-24'>
+
+            <div className='col-span-12'>
+              <FeaturedProject projectImg={RandSConstructionsWebsiteProj} projectType='Featured Project' projectTitle='R & S Constructions LLC Website'
+                projectDescription='Fully Responsive Website using JavaScript, HTML, and CSS utilizing React JavaScript Framework. R & S Constructions LLC is a full-service residential design and build remodeling firm. Specializing in new construction, custom home building and residential renovations in the Maryland area.'
+                gitHubURL='https://github.com/ryanabeysinghe/RandSConstructionsLLC-Website' projectURL='https://randsconstructions.com/' />
+            </div>
+
+            <div className='col-span-6'>
+              Project-1
+            </div>
+
+            <div className='col-span-6'>
+              Project-2
+            </div>
+
+            <div className='col-span-12'>
+              Featured Project
+            </div>
+
+            <div className='col-span-6'>
+              Project-3
+            </div>
+
+            <div className='col-span-6'>
+              Project-4
+            </div>
+
+          </div>
+
+
+        </Layout>
+      </main>
+    </>
+  );
+};
+
+export default projects;
