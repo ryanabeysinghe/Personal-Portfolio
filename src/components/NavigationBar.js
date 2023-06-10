313fdd0308e25adfe539e9e08ce13d3b6cf8300f
Ryan Abeysinghe <ryanabey@terpmail.umd.edu>
2023-06-10 12:38:58 -0400

    setting up light/dark mode

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -2,10 +2,12 @@ import React from 'react'
 import Link from 'next/link'
 import Logo from '@/components/Logo'
 import { useRouter } from 'next/router'
-import { GithubIcon, InstagramIcon, LinkedInIcon, YouTubeIcon } from '@/components/SocialMediaIcons'
+import { GithubIcon, InstagramIcon, LinkedInIcon, MoonIcon, SunIcon, YouTubeIcon } from '@/components/SocialMediaIcons'
 import { motion } from 'framer-motion'
+import { useTheme } from 'styled-components'
+import useSwitchTheme from './hooks/useSwitchTheme'
 
-const CustomLink = ({href, title, className=""}) => {
+const CustomLink = ({ href, title, className = "" }) => {
     const router = useRouter();
 
     return (
@@ -18,33 +20,45 @@ const CustomLink = ({href, title, className=""}) => {
 };
 
 const NavigationBar = () => {
-  return (
-    <header className='w-full px-32 py-9 font-medium flex items-center justify-between border-b-2 border-solid border-dark'>
-        <nav>
-            <CustomLink href="/" title="Home" className='mr-4 uppercase'/>
-            <CustomLink href="/about" title="About" className='mx-4 uppercase'/>
-            <CustomLink href="/projects" title="Projects" className='mx-4 uppercase'/>
-            <CustomLink href="/contact" title="Contact" className='ml-4 uppercase'/>
-        </nav>
 
-        <nav className='flex items-center justify-center flex-wrap'>
-            <motion.a href="https://www.linkedin.com/in/ryan-abeysinghe/" target={"_blank"} whileHover={{y: -3}} whileTap={{scale: 0.8}} className='w-6 mr-3'>
-                <LinkedInIcon />
-            </motion.a>
-            <motion.a href="https://github.com/ryanabeysinghe" target={"_blank"} whileHover={{y: -3}} whileTap={{scale: 0.8}} className='w-6 mx-3'>
-                <GithubIcon />
-            </motion.a>
-            <motion.a href="https://www.instagram.com/ryanabeysinghe/" target={"_blank"} whileHover={{y: -3}} whileTap={{scale: 0.8}} className='w-6 ml-3'>
-                <InstagramIcon />
-            </motion.a>
-        </nav>
+    const [mode, setMode] = useSwitchTheme();
 
-        <div className='absolute left-[50%] top-2 translate-x-[-50%]'>
-            <Logo />
-        </div>
+    return (
+        <header className='w-full px-32 py-9 font-medium flex items-center justify-between border-b-2 border-solid border-dark'>
+            <nav>
+                <CustomLink href="/" title="Home" className='mr-4 uppercase' />
+                <CustomLink href="/about" title="About" className='mx-4 uppercase' />
+                <CustomLink href="/projects" title="Projects" className='mx-4 uppercase' />
+                <CustomLink href="/contact" title="Contact" className='ml-4 uppercase' />
+            </nav>
 
-    </header>
-  )
+            <nav className='flex items-center justify-center flex-wrap'>
+                <motion.a href="https://www.linkedin.com/in/ryan-abeysinghe/" target={"_blank"} whileHover={{ y: -3 }} whileTap={{ scale: 0.8 }} className='w-6 mr-3'>
+                    <LinkedInIcon />
+                </motion.a>
+                <motion.a href="https://github.com/ryanabeysinghe" target={"_blank"} whileHover={{ y: -3 }} whileTap={{ scale: 0.8 }} className='w-6 mx-3'>
+                    <GithubIcon />
+                </motion.a>
+                <motion.a href="https://www.instagram.com/ryanabeysinghe/" target={"_blank"} whileHover={{ y: -3 }} whileTap={{ scale: 0.8 }} className='w-6 ml-3'>
+                    <InstagramIcon />
+                </motion.a>
+
+                <button onClick={ () => setMode(mode === 'light' ? 'dark' : 'light') } className='ml-3 flex items-center justify-center rounded-full p-1'>
+                    {
+                        mode === 'dark' ?
+                        <SunIcon className='fill-dark' /> : <MoonIcon className='fill-dark' />
+                    }
+
+                </button>
+
+            </nav>
+
+            <div className='absolute left-[50%] top-2 translate-x-[-50%]'>
+                <Logo />
+            </div>
+
+        </header>
+    )
 }
 
 export default NavigationBar
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -17,7 +17,7 @@ export default function App({ Component, pageProps }) {
       <meta name="viewport" content="width=device-width, initial-scale=1" />
     </Head>
 
-    <main className={`${montserratFont.variable} font-sans bg-light w-full min-h-screen`}>
+    <main className={`${montserratFont.variable} font-sans bg-light dark:bg-dark w-full min-h-screen`}>
       <NavigationBar />
       <Component {...pageProps} />
       <Footer />
