d47329773fc9a08610b92520d27615c8a5f09a29
Ryan Abeysinghe <ryanabey@terpmail.umd.edu>
2023-06-11 00:07:28 -0400

    setting up responsive navbar

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Link from 'next/link'
 import Logo from '@/components/Logo'
 import { useRouter } from 'next/router'
@@ -22,36 +22,50 @@ const CustomLink = ({ href, title, className = "" }) => {
 const NavigationBar = () => {
 
     const [mode, setMode] = useSwitchTheme();
+    const [isOpen, setIsOpen] = useState(false);
+
+    const handleClick = () => {
+        setIsOpen(!isOpen)
+    }
 
     return (
         <header className='w-full px-32 py-9 font-medium flex items-center justify-between border-b-2 border-solid border-dark dark:text-light dark:border-light'>
-            <nav>
-                <CustomLink href="/" title="Home" className='mr-4 uppercase' />
-                <CustomLink href="/about" title="About" className='mx-4 uppercase' />
-                <CustomLink href="/projects" title="Projects" className='mx-4 uppercase' />
-                <CustomLink href="/contact" title="Contact" className='ml-4 uppercase' />
-            </nav>
 
-            <nav className='flex items-center justify-center flex-wrap'>
-                <motion.a href="https://www.linkedin.com/in/ryan-abeysinghe/" target={"_blank"} whileHover={{ y: -3 }} whileTap={{ scale: 0.8 }} className='w-6 mr-3'>
-                    <LinkedInIcon />
-                </motion.a>
-                <motion.a href="https://github.com/ryanabeysinghe" target={"_blank"} whileHover={{ y: -3 }} whileTap={{ scale: 0.8 }} className='w-6 mx-3'>
-                    <GithubIcon />
-                </motion.a>
-                <motion.a href="https://www.instagram.com/ryanabeysinghe/" target={"_blank"} whileHover={{ y: -3 }} whileTap={{ scale: 0.8 }} className='w-6 ml-3'>
-                    <InstagramIcon />
-                </motion.a>
+            <button className='flex-col justify-center items-center hidden lg:flex' onClick={handleClick}>
+                <span className={`bg-dark dark:bg-light block transition-all duration-300 ease-out h-0.5 w-6 rounded-sm -translate-y-0.5 ${isOpen ? 'rotate-45 translate-y-1' : '-translate-y-0.5'}`}></span>
+                <span className={`bg-dark dark:bg-light block transition-all duration-300 ease-out h-0.5 w-6 rounded-sm my-0.5 ${isOpen ? 'opacity-0' : 'opacity-100'}`}></span>
+                <span className={`bg-dark dark:bg-light block transition-all duration-300 ease-out h-0.5 w-6 rounded-sm translate-y-0.5 ${isOpen ? '-rotate-45 -translate-y-1' : 'translate-y-0.5'}`}></span>
+            </button>
 
-                <button onClick={ () => setMode(mode === 'light' ? 'dark' : 'light') } className={`ml-3 flex items-center justify-center rounded-full p-1 ${mode === 'light' ? 'bg-dark text-light' : 'bg-light text-dark'} `}>
-                    {
-                        mode === 'dark' ?
-                        <SunIcon className='fill-dark' /> : <MoonIcon className='fill-dark' />
-                    }
+            <div className='w-full flex justify-between items-center lg:hidden'>
+                <nav>
+                    <CustomLink href="/" title="home" className='mr-4' />
+                    <CustomLink href="/about" title="about" className='mx-4' />
+                    <CustomLink href="/projects" title="projects" className='mx-4' />
+                    <CustomLink href="/contact" title="contact" className='ml-4' />
+                </nav>
 
-                </button>
+                <nav className='flex items-center justify-center flex-wrap'>
+                    <motion.a href="https://www.linkedin.com/in/ryan-abeysinghe/" target={"_blank"} whileHover={{ y: -3 }} whileTap={{ scale: 0.8 }} className='w-6 mr-3'>
+                        <LinkedInIcon />
+                    </motion.a>
+                    <motion.a href="https://github.com/ryanabeysinghe" target={"_blank"} whileHover={{ y: -3 }} whileTap={{ scale: 0.8 }} className='w-6 mx-3'>
+                        <GithubIcon />
+                    </motion.a>
+                    <motion.a href="https://www.instagram.com/ryanabeysinghe/" target={"_blank"} whileHover={{ y: -3 }} whileTap={{ scale: 0.8 }} className='w-6 ml-3'>
+                        <InstagramIcon />
+                    </motion.a>
 
-            </nav>
+                    <button onClick={() => setMode(mode === 'light' ? 'dark' : 'light')} className={`ml-3 flex items-center justify-center rounded-full p-1 ${mode === 'light' ? 'bg-dark text-light' : 'bg-light text-dark'} `}>
+                        {
+                            mode === 'dark' ?
+                                <SunIcon className='fill-dark' /> : <MoonIcon className='fill-dark' />
+                        }
+
+                    </button>
+
+                </nav>
+            </div>
 
             <div className='absolute left-[50%] top-2 translate-x-[-50%]'>
                 <Logo />
--- tailwind.config.js
+++ tailwind.config.js
@@ -15,7 +15,7 @@ module.exports = {
         light: "#FFF1E6", // Coconut
         primary: "#FF2400", // Scarlet 
         primaryDark: "#E3735E", // Terra Cotta
-        
+
         // dark: "#1b1b1b",
         // light: "#f5f5f5",
         // primary: "#B63E96", // 240,86,199
@@ -37,6 +37,25 @@ module.exports = {
           '100%': { transform: 'translateX(0%)' },
         },
       },
+    },
+    screens: {
+      '2xl': { max: '1535px' },
+      // => @media (max-width: 1535px) { ... }
+
+      xl: { max: '1279px' },
+      // => @media (max-width: 1279px) { ... }
+
+      lg: { max: '1023px' },
+      // => @media (max-width: 1023px) { ... }
+
+      md: { max: '767px' },
+      // => @media (max-width: 767px) { ... }
+
+      sm: { max: '639px' },
+      // => @media (max-width: 639px) { ... }
+
+      xs: { max: '479px' },
+      // => @media (max-width: 479px) { ... }
     },
   },
   plugins: [],
