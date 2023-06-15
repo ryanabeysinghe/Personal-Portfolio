5be2a15ce65a4224b034f00eb713dfcade87f0d4
Ryan Abeysinghe <ryanabey@terpmail.umd.edu>
2023-06-15 12:40:47 -0400

    fixed animation timeline

--- src/components/ProgressIndicator.js
+++ src/components/ProgressIndicator.js
@@ -5,7 +5,7 @@ const ProgressIndicator = ({reference}) => {
 
     const { scrollYProgress } = useScroll({
         target: reference,
-        offset: ["center end", "end center"]
+        offset: ["start end", "end center"]
     })
 
     return (
