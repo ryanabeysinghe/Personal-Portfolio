31890571a943cdaf35e4ddfbdcf1b37bff26b456
Ryan Abeysinghe <ryanabey@terpmail.umd.edu>
2023-06-15 14:53:21 -0400

    setting up max width resolution for larger devices

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -9,6 +9,7 @@ import NavigationBar from '@/components/NavigationBar';
 import Footer from '@/components/Footer'
 import { ThemeProvider } from '@/components/ThemeContext';
 
+
 const montserratFont = Montserrat({
   subsets: ['latin'],
   variable: '--font-montserrat'
@@ -26,13 +27,25 @@ export default function App({ Component, pageProps }) {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={`${montserratFont.variable} font-sans bg-light dark:bg-dark w-full min-h-screen`}>
+      {/* <main className={`${montserratFont.variable} max-w-[1920px] mx-auto font-sans bg-light dark:bg-dark w-full min-h-screen`}>
         <ThemeProvider>
           <NavigationBar />
           <AnimatePresence initial={false} mode='wait'>
             <Component key={router.asPath} {...pageProps} />
           </AnimatePresence>
           <Footer />
+        </ThemeProvider>
+      </main> */}
+
+      <main className={`${montserratFont.variable} font-sans bg-light dark:bg-dark w-full min-h-screen`}>
+        <ThemeProvider>
+          <div className='max-w-[1920px] mx-auto'>
+            <NavigationBar />
+            <AnimatePresence initial={false} mode='wait'>
+              <Component key={router.asPath} {...pageProps} />
+            </AnimatePresence>
+            <Footer />
+          </div>
         </ThemeProvider>
       </main>
     </>
