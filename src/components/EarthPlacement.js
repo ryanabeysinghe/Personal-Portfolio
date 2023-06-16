4eb77ed57ef547027f175984c246d2033ed591e6
Ryan Abeysinghe <ryanabey@terpmail.umd.edu>
2023-06-15 21:18:33 -0400

    earth animation done

---
+++ src/components/EarthPlacement.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import EarthCanvas from './Earth';
+
+const EarthPlacement = () => {
+  return (
+    <div className='flex fixed right-4 bottom-16 items-center justify-center overflow-hidden'>
+        <div className='w-48 h-auto flex items-center justify-center relative cursor-pointer md:hidden'>
+            <EarthCanvas />
+        </div>
+    </div>
+  );
+};
+
+export default EarthPlacement;
