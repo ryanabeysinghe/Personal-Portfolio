2ec6c77dc640f49944247c8d11e1f8cd4459850f
Ryan Abeysinghe <ryanabey@terpmail.umd.edu>
2023-06-16 13:39:51 -0400

    page scroll animation done

--- src/pages/about.js
+++ src/pages/about.js
@@ -61,7 +61,7 @@ const About = () => {
 
             <PageTransition />
 
-            <motion.div className='fixed top-0 left-0 right-0 h-[0.5rem] bg-primary origin-[0%] z-10' style={{ scaleX: scrollYProgress }} />
+            <motion.div className='fixed top-0 left-0 right-0 h-[0.5rem] bg-primary origin-[0%] z-40' style={{ scaleX: scrollYProgress }} />
 
             <main className='flex w-full flex-col items-center justify-center'>
                 <Layout className='pt-16 xl:pt-16'>
