e0e14b00ba8b8c7c13d642d0384984b5ccf4a26a
Ryan Abeysinghe <ryanabey@terpmail.umd.edu>
2023-06-20 13:02:47 -0400

    minor ui changes to contact page

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import Confetti from 'react-confetti';
 import { motion, useScroll } from 'framer-motion';
 import { Button, Container, FormControl, FormErrorMessage, FormLabel, Input, Text, Textarea, useToast, useColorModeValue } from '@chakra-ui/react';
 import Head from 'next/head';
@@ -8,6 +9,7 @@ import Layout from '@/components/Layout';
 import TextAnimation from '@/components/TextAnimation';
 import { useTheme } from '@/components/ThemeContext';
 import { sendContactForm } from '@/lib/api';
+import RA from '@/components/RA';
 
 const initValues = { name: "", email: "", subject: "", message: "" };
 
@@ -17,6 +19,8 @@ const Contact = () => {
 
   const { scrollYProgress } = useScroll();
 
+  const toast = useToast();
+
   const [state, setState] = useState(initialState);
   const [touched, setTouched] = useState({});
 
@@ -24,12 +28,12 @@ const Contact = () => {
 
   const onBlur = ({ target }) => setTouched((prev) => ({ ...prev, [target.name]: true }));
 
-  const toast = useToast();
-
   const [resize, setResize] = React.useState('vertical');
 
   const { mode } = useTheme();
 
+  const [showConfetti, setShowConfetti] = useState(false);
+ 
   const handleChange = ({ target }) =>
     setState((prev) => ({
       ...prev,
@@ -52,9 +56,10 @@ const Contact = () => {
       toast({
         title: "Message Sent.",
         status: "success",
-        duration: 2000,
+        duration: 3000,
         position: "top",
       });
+      setShowConfetti(true);
     } catch (error) {
       setState((prev) => ({
         ...prev,
@@ -82,50 +87,53 @@ const Contact = () => {
       <main className='w-full flex flex-col items-center justify-center mb-16'>
         <Layout className='pt-16'>
 
-          <TextAnimation text="Get In Touch!" className='mb-16 2xl:!text-7xl xl:!text-6xl md:!text-5xl xs:!text-4xl xxxs:!text-3xl sm:mb-8' />
+          <TextAnimation text="Get In Touch!" className='mb-16 !text-7xl lg:!text-7xl md:!text-6xl sm:!text-5xl xs:!text-4xl xxs:!text-3xl sm:mb-8' />
 
           {/* <Container> */}
-          <div className='w-full max-w-[650px] mx-auto flex flex-col h-auto relative items-center justify-between border-2 border-solid rounded-br-3xl rounded-3xl shadow-2xl p-12 border-dark bg-light dark:border-light dark:bg-dark'>
+          <div className='w-full max-w-[650px] mx-auto flex flex-col h-auto relative items-center justify-between border-2 border-solid rounded-br-3xl rounded-3xl shadow-2xl p-12 border-dark bg-light dark:border-light dark:bg-dark md:max-w-[550px] sm:max-w-[450px] xs:max-w-[350px] xs:p-8 xxs:max-w-[300px] xxxs:p-6 '>
 
           <div className='absolute top-0 -right-3 -z-10 w-[101%] h-[103%] rounded-[2.5rem] rounded-br-3xl bg-dark dark:bg-light' />
 
             {error && (
-              <Text style={{color: "#FA8072"}} mt={-8} mb={8} fontSize="xl" fontWeight='semibold'>
+              <Text style={{color: "#FA8072"}} mb={8} fontSize={["xs", "sm", "md", "lg"]} fontWeight='semibold'>
                 {error}
               </Text>
             )}
 
             <FormControl isRequired isInvalid={touched.name && !values.name} mb={8}>
               {/* <FormLabel>Name</FormLabel> */}
-              <Input type='text' name='name' placeholder='Name *' _placeholder={{ color: `${mode === 'light' ? '#28282B' : '#FFF1E6'}` }} style={{ color: `${mode === 'light' ? '#28282B' : '#FFF1E6'}` }} fontWeight='medium' fontSize={["sm", "md", "lg", "xl"]} borderColor={{ color: `${mode === 'light' ? '#28282B' : '#FFF1E6'}` }} _hover={{ borderColor: `${mode === 'light' ? '#FA8072' : '#FA8072'}` }} borderWidth='2px' value={values.name} onChange={handleChange} onBlur={onBlur} />
+              <Input type='text' name='name' placeholder='Name *' _placeholder={{ color: `${mode === 'light' ? '#808080' : '#808080'}` }} style={{ color: `${mode === 'light' ? '#28282B' : '#FFF1E6'}` }} fontWeight='medium' fontSize={["xs", "sm", "md", "lg"]} borderColor={{ color: `${mode === 'light' ? '#28282B' : '#FFF1E6'}` }} _hover={{ borderColor: `${mode === 'light' ? '#FA8072' : '#FA8072'}` }} borderWidth='2px' value={values.name} onChange={handleChange} onBlur={onBlur} />
 
               <FormErrorMessage style={{ color: "#FA8072" }} fontWeight='semibold' fontSize='sm' mt={4}>Name field is required</FormErrorMessage>
             </FormControl>
 
             <FormControl isRequired isInvalid={touched.email && !values.email} mb={8}>
               {/* <FormLabel>Email</FormLabel> */}
-              <Input type='email' name='email' placeholder='Email *' _placeholder={{ color: `${mode === 'light' ? '#28282B' : '#FFF1E6'}` }} style={{ color: `${mode === 'light' ? '#28282B' : '#FFF1E6'}` }} fontWeight='medium' fontSize={["sm", "md", "lg", "xl"]} borderColor={{ color: `${mode === 'light' ? '#28282B' : '#FFF1E6'}` }} _hover={{ borderColor: `${mode === 'light' ? '#FA8072' : '#FA8072'}` }} borderWidth='2px' value={values.email} onChange={handleChange} onBlur={onBlur} />
+              <Input type='email' name='email' placeholder='Email *' _placeholder={{ color: `${mode === 'light' ? '#808080' : '#808080'}` }} style={{ color: `${mode === 'light' ? '#28282B' : '#FFF1E6'}` }} fontWeight='medium' fontSize={["xs", "sm", "md", "lg"]} borderColor={{ color: `${mode === 'light' ? '#28282B' : '#FFF1E6'}` }} _hover={{ borderColor: `${mode === 'light' ? '#FA8072' : '#FA8072'}` }} borderWidth='2px' value={values.email} onChange={handleChange} onBlur={onBlur} />
               <FormErrorMessage style={{ color: "#FA8072" }} fontWeight='semibold' fontSize='sm' mt={4}>Email field is required</FormErrorMessage>
             </FormControl>
 
             <FormControl isRequired isInvalid={touched.subject && !values.subject} mb={8}>
               {/* <FormLabel>Subject</FormLabel> */}
-              <Input type='text' name='subject' placeholder='Subject *' _placeholder={{ color: `${mode === 'light' ? '#28282B' : '#FFF1E6'}` }} style={{ color: `${mode === 'light' ? '#28282B' : '#FFF1E6'}` }} fontWeight='medium' fontSize={["sm", "md", "lg", "xl"]} borderColor={{ color: `${mode === 'light' ? '#28282B' : '#FFF1E6'}` }} _hover={{ borderColor: `${mode === 'light' ? '#FA8072' : '#FA8072'}` }} borderWidth='2px' value={values.subject} onChange={handleChange} onBlur={onBlur} />
+              <Input type='text' name='subject' placeholder='Subject *' _placeholder={{ color: `${mode === 'light' ? '#808080' : '#808080'}` }} style={{ color: `${mode === 'light' ? '#28282B' : '#FFF1E6'}` }} fontWeight='medium' fontSize={["xs", "sm", "md", "lg"]} borderColor={{ color: `${mode === 'light' ? '#28282B' : '#FFF1E6'}` }} _hover={{ borderColor: `${mode === 'light' ? '#FA8072' : '#FA8072'}` }} borderWidth='2px' value={values.subject} onChange={handleChange} onBlur={onBlur} />
               <FormErrorMessage style={{ color: "#FA8072" }} fontWeight='semibold' fontSize='sm'mt={4}>Subject field is required</FormErrorMessage>
             </FormControl>
 
             <FormControl isRequired isInvalid={touched.message && !values.message} mb={8}>
               {/* <FormLabel>Message</FormLabel> */}
-              <Textarea type='text' name='message' placeholder='Message *' _placeholder={{ color: `${mode === 'light' ? '#28282B' : '#FFF1E6'}` }} style={{ color: `${mode === 'light' ? '#28282B' : '#FFF1E6'}` }} fontWeight='medium' fontSize={["sm", "md", "lg", "xl"]} height="200px" resize={resize} borderColor={{ color: `${mode === 'light' ? '#28282B' : '#FFF1E6'}` }} _hover={{ borderColor: `${mode === 'light' ? '#FA8072' : '#FA8072'}` }} borderWidth='2px' rows={4} value={values.message} onChange={handleChange} onBlur={onBlur} />
+              <Textarea type='text' name='message' placeholder='Message *' _placeholder={{ color: `${mode === 'light' ? '#808080' : '#808080'}` }} style={{ color: `${mode === 'light' ? '#28282B' : '#FFF1E6'}` }} fontWeight='medium' fontSize={["xs", "sm", "md", "lg"]} resize={resize} borderColor={{ color: `${mode === 'light' ? '#28282B' : '#FFF1E6'}` }} _hover={{ borderColor: `${mode === 'light' ? '#FA8072' : '#FA8072'}` }} borderWidth='2px' rows={8} value={values.message} onChange={handleChange} onBlur={onBlur} />
               <FormErrorMessage style={{ color: "#FA8072" }} fontWeight='semibold' fontSize='sm' mt={4}>Message field is required</FormErrorMessage>
             </FormControl>
 
-            <Button variant='outline' style={{ color: "#FA8072" }} fontWeight='bold' fontSize={["xs", "sm", "md", "lg"]} width='200px' height='50px' borderColor={{ color: "#FA8072" }} borderWidth='2px' _hover={{ backgroundColor: `${mode === 'light' ? '#28282B' : '#FFF1E6'}` }} isLoading={isLoading} disabled={!values.name || !values.email || !values.subject || !values.message} onClick={onSubmit}>SUBMIT</Button>
+            <Button variant='outline' style={{ color: "#FA8072" }} fontWeight='bold' fontSize={["xs", "sm", "md", "lg"]} width={["80px", "140px", "200px", "200px"]} height={["30px", "40px", "50px", "50px"]} borderColor={{ color: "#FA8072" }} borderWidth='2px' _hover={{ backgroundColor: `${mode === 'light' ? '#28282B' : '#FFF1E6'}` }} isLoading={isLoading} disabled={!values.name || !values.email || !values.subject || !values.message} onClick={onSubmit}>SUBMIT</Button>
 
           </div>
+          {showConfetti && <Confetti width={window.screen.width} height={window.screen.height} />} {/* Render confetti when showConfetti is true */}
           {/* </Container> */}
 
         </Layout>
+
+        <RA />
       </main>
     </>
   );
